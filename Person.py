a5bc042ad4a2b38227c7bec6f9b7021557e251dd
Alexander Khalil <khalilal@mnstate.edu>
2017-04-24 12:05:18 -0500

    created constructor Method

--- Person.py
+++ Person.py
@@ -1,2 +1,7 @@
 class Person:
   lastIDUsed = 100
+  def __init__(self, fName, lName, ID):
+    self._fName = fName
+    self._lName = lName
+    self._ID = Person.lastIDUsed
+    Person.lastIDUsed += 1
