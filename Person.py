8d84f127647792bf7f3b5c2d76fbccf6ff17aa09
Alexander Khalil <khalilal@mnstate.edu>
2017-04-24 12:08:05 -0500

    test the contructor and fixed parameters for constructor

--- Person.py
+++ Person.py
@@ -1,7 +1,8 @@
 class Person:
   lastIDUsed = 100
-  def __init__(self, fName, lName, ID):
+  def __init__(self, fName, lName):
     self._fName = fName
     self._lName = lName
     self._ID = Person.lastIDUsed
     Person.lastIDUsed += 1
+p1 = Person('Joe', 'Jones')
