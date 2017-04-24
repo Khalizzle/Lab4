fa7b44f28db3d0e8721b08d78c3d72ea0c381afa
Alexander Khalil <khalilal@mnstate.edu>
2017-04-24 12:21:08 -0500

    add str and test in the method

--- Person.py
+++ Person.py
@@ -1,2 +1,8 @@
 class Person:
   lastIDUsed = 100
+  def __str__(self):
+    tmp = ''
+    tmp += 'Name' + str(self._lName) + ',' + str(self._fName)
+    return tmp
+
+print(Person('Greg', 'Mandice'))
